b8bebb2037a1e8661bf90a2f31928b4081236748
swagranger011 <abhisht01@gmail.com>
2024-12-08 14:13:30 -0600

    Added sun & moon icons

--- BudgetMate/client/src/Mode.jsx
+++ BudgetMate/client/src/Mode.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useMode } from "./ModeContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 import "./Mode.css";
 
 const Mode = () => {
@@ -13,7 +15,15 @@ const Mode = () => {
           checked={isDarkMode}
           onChange={toggleMode}
         />
-        <span className="slider theme"></span>
+        <span className="slider theme">
+          {/* Icons inside the slider */}
+          <span className="icon sun">
+            <FontAwesomeIcon icon={faSun} />
+          </span>
+          <span className="icon moon">
+            <FontAwesomeIcon icon={faMoon} />
+          </span>
+        </span>
       </label>
     </div>
   );
